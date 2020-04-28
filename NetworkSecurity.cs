0fe41c6a33a009d89b3d677c623bc9a65f846346
Perillio <47478204+Perillio@users.noreply.github.com>
2020-04-28 22:01:42 +0200

    Create NetworkSecurity.cs

---
+++ NetworkSecurity.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using UnityEngine;
+
+// Cryptics available in this class: SHA256 Hash, SHA512 Hash, MD5 Hash, Rijndael Encryption and Decryption (PBKDF2).
+
+public class NetworkSecurity : MonoBehaviour
+{
+
+    public string Sha256(string randomString)
+    {
+        var crypt = new SHA256Managed();
+        string hash = string.Empty;
+        byte[] crypto = crypt.ComputeHash(Encoding.ASCII.GetBytes(randomString));
+        foreach (byte theByte in crypto)
+        {
+            hash += theByte.ToString("x2");
+        }
+        return hash;
+    }
+    public string MD5(string data)
+    {
+        MD5 md5 = new MD5CryptoServiceProvider();
+        md5.ComputeHash(ASCIIEncoding.ASCII.GetBytes(data));
+        byte[] result = md5.Hash;
+        StringBuilder strBuilder = new StringBuilder();
+        for (int i = 0; i < result.Length; i++)
+        {
+            strBuilder.Append(result[i].ToString("x2"));
+        }
+        return strBuilder.ToString();
+    }
+    public string Sha512(string randomString)
+    {
+        var crypt = new SHA512Managed();
+        string hash = string.Empty;
+        byte[] crypto = crypt.ComputeHash(Encoding.ASCII.GetBytes(randomString));
+        foreach (byte theByte in crypto)
+        {
+            hash += theByte.ToString("x2");
+        }
+        return hash;
+    }
+    private const int Keysize = 256;
+    private const int DerivationIterations = 1000;
+    public string Encrypt(string plainText, string passPhrase)
+    {
+        var saltStringBytes = Generate256BitsOfRandomEntropy();
+        var ivStringBytes = Generate256BitsOfRandomEntropy();
+        var plainTextBytes = Encoding.UTF8.GetBytes(plainText);
+        using (var password = new Rfc2898DeriveBytes(passPhrase, saltStringBytes, DerivationIterations))
+        {
+            var keyBytes = password.GetBytes(Keysize / 8);
+            using (var symmetricKey = new RijndaelManaged())
+            {
+                symmetricKey.BlockSize = 256;
+                symmetricKey.Mode = CipherMode.CBC;
+                symmetricKey.Padding = PaddingMode.PKCS7;
+                using (var encryptor = symmetricKey.CreateEncryptor(keyBytes, ivStringBytes))
+                {
+                    using (var memoryStream = new MemoryStream())
+                    {
+                        using (var cryptoStream = new CryptoStream(memoryStream, encryptor, CryptoStreamMode.Write))
+                        {
+                            cryptoStream.Write(plainTextBytes, 0, plainTextBytes.Length);
+                            cryptoStream.FlushFinalBlock();
+                            var cipherTextBytes = saltStringBytes;
+                            cipherTextBytes = cipherTextBytes.Concat(ivStringBytes).ToArray();
+                            cipherTextBytes = cipherTextBytes.Concat(memoryStream.ToArray()).ToArray();
+                            memoryStream.Close();
+                            cryptoStream.Close();
+                            return Convert.ToBase64String(cipherTextBytes);
+                        }
+                    }
+                }
+            }
+        }
+    }
+    public string Decrypt(string cipherText, string passPhrase)
+    {
+        var cipherTextBytesWithSaltAndIv = Convert.FromBase64String(cipherText);
+        var saltStringBytes = cipherTextBytesWithSaltAndIv.Take(Keysize / 8).ToArray();
+        var ivStringBytes = cipherTextBytesWithSaltAndIv.Skip(Keysize / 8).Take(Keysize / 8).ToArray();
+        var cipherTextBytes = cipherTextBytesWithSaltAndIv.Skip((Keysize / 8) * 2).Take(cipherTextBytesWithSaltAndIv.Length - ((Keysize / 8) * 2)).ToArray();
+        using (var password = new Rfc2898DeriveBytes(passPhrase, saltStringBytes, DerivationIterations))
+        {
+            var keyBytes = password.GetBytes(Keysize / 8);
+            using (var symmetricKey = new RijndaelManaged())
+            {
+                symmetricKey.BlockSize = 256;
+                symmetricKey.Mode = CipherMode.CBC;
+                symmetricKey.Padding = PaddingMode.PKCS7;
+                using (var decryptor = symmetricKey.CreateDecryptor(keyBytes, ivStringBytes))
+                {
+                    using (var memoryStream = new MemoryStream(cipherTextBytes))
+                    {
+                        using (var cryptoStream = new CryptoStream(memoryStream, decryptor, CryptoStreamMode.Read))
+                        {
+                            var plainTextBytes = new byte[cipherTextBytes.Length];
+                            var decryptedByteCount = cryptoStream.Read(plainTextBytes, 0, plainTextBytes.Length);
+                            memoryStream.Close();
+                            cryptoStream.Close();
+                            return Encoding.UTF8.GetString(plainTextBytes, 0, decryptedByteCount);
+                        }
+                    }
+                }
+            }
+        }
+    }
+    private static byte[] Generate256BitsOfRandomEntropy()
+    {
+        var randomBytes = new byte[32];
+        using (var rngCsp = new RNGCryptoServiceProvider())
+        {
+            rngCsp.GetBytes(randomBytes);
+        }
+        return randomBytes;
+    }
+}
